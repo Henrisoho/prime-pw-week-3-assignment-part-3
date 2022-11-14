6b39137da4277e45f84f18eddd2c50e854904274
henrisoho <bakingwithmymother@gmail.com>
2022-11-13 23:11:33 -0600

    1-5

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -10,6 +10,7 @@ console.log( partsNeeded )
 //    the following numbers: 3, 5, -6, 0, 7, 11
 console.log('2. Array of supplyChanges:');
 const supplyChanges = [3, 5, -6, 0, 7, 11];
+console.log( supplyChanges )
 // 3. Console log the value of the second item in the 'supplyChanges' array
 console.log('3. Second supplyChange is:');
     console.log(supplyChanges[1])
@@ -18,10 +19,13 @@ console.log('3. Second supplyChange is:');
 //    array & console.log the value removed.
 console.log('4. Removed item:');
 supplyChanges.pop
+console.log(supplyChanges)
 
 // 5. A delivery of 25 more parts arrived. Add the value 25 to the end of the array
 console.log('5. Adding 25 to supplyChanges.');
 supplyChanges.push(25)
+console.log(supplyChanges)
+
 // 6. Write a `for` loop that shows each value in the 'supplyChanges' array
 //    Use a console.log formatted as follows, where x is the value from the array
 //    - if it is a positive number (greater than 0), log 'Added x parts.' 
